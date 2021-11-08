81189f2220823a982cfb4cdf8caca077b4b18a8b
Wasanta <kwan_u_day@hotmail.com>
2021-11-08 09:49:31 -0500

    completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    let input = document.querySelector('input');
+    input.addEventListener('click',clickAlert)
 }
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
+addingEventListener();
